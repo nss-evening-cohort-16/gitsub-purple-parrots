05bf3bd76bf559b1212f8615812e7503d243078b
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2021-08-09 19:55:15 -0500

    setup shared layout and modularized

---
+++ sharedLayout.js
@@ -0,0 +1,77 @@
+import { renderToDom } from "./renderToDom.js";
+
+//function to render the nav bar
+const createNavBar = () => {
+  let domString = `
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Overview</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Repositories</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Packages</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  `;
+
+  renderToDom("#navbar", domString);
+};
+
+//function to render profile area
+const createProfile = () => {
+  let domString = `
+  <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+  `;
+  renderToDom("#profile", domString);
+};
+
+//function to render footer
+const createFooter = () => {
+  let domString = `
+  <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
+  `;
+  renderToDom("#footer", domString);
+};
+
+
+export { createNavBar, createProfile, createFooter };
