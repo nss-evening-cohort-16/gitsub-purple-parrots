0a2fd1e94ff3c2218c8b529bfaf4bc0e20bf59bc
Klay Thacker <klaythacker11@gmail.com>
2021-08-10 19:06:06 -0500

    created form, array,repoContainer,repoBuilder for repo.js created array for repo added if statment in main js

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 import { createNavBar, createProfile, createFooter } from "./sharedLayout.js";
+import { createRepoForm, createRepoContainer, createRepo } from "./repositories.js";
 
 const init = () => {
   createNavBar();
   createProfile();
   createFooter();
+  
+  if (window.location.pathname.includes("repositories")) {
+    createRepoForm();
+    createRepoContainer(createRepo);
+  }
 };
 
 init();
--- repositories.js
+++ repositories.js
@@ -1,2 +1,54 @@
 import { renderToDom } from "./renderToDom.js";
-import { createNavBar, createProfile, createFooter } from "./main.js"
+import { createNavBar, createProfile, createFooter } from "./sharedLayout.js"
+
+export const createRepo = [
+{
+  name: "pies",
+  description: "the big pie",
+}
+];
+
+const createRepoForm = () => {
+    let domString = `
+    <form>
+    <div class="mb-3">
+        <label for="repoInput1" class="repo-label">Repository Name</label>
+        <input type="text" class="form-control" id="repoInput1" aria-describedby="repoHelp">
+      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+      <div class="mb-3">
+        <label for="inputDescription1" class="form-label">Description</label>
+        <input type="text" class="form-control" id="inputDescription1">
+    </div>
+    <button type="button" id="formButton" class="btn btn-primary">Create Repository</button>
+</form>
+    `
+    renderToDom("#container2" ,domString);
+};
+
+const createRepoContainer = () => {
+  let domString = `
+  
+  `
+  renderToDom("#container1" ,domString);
+
+}
+const repoBuilder = (array) => {
+    let domString = "";
+    array.forEach((object, i) => {
+        domString += `
+        <div class="card" style="width: 20rem;">
+          <div class="card-body">
+            <h5 class="card-title">${object.name}</h5>
+            <p class="card-text">${object.house}</p>
+              
+              <button type="button" id=${i} id="expelButton" class="btn btn-primary">Expel</button>
+          </div>
+        </div>
+        `
+      renderToDom("#studentCard", domString);
+      const expel = document.querySelector("#expelButton");
+      expel.addEventListener("click", expelStudent);
+}
+)};
+export { createRepoForm, createRepoContainer }
