45de7183ef8800a36abd4637847c744f620141bb
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2021-08-14 13:00:40 -0500

    added complex data and edited the profile area

--- sharedLayout.js
+++ sharedLayout.js
@@ -1,4 +1,5 @@
 import { renderToDom } from "./renderToDom.js";
+import { userProfileData } from "./userProfileData.js";
 
 //function to render the nav bar
 const createNavBar = () => {
@@ -30,24 +31,30 @@ const createNavBar = () => {
 };
 
 //function to render profile area
-const createProfile = () => {
-  let domString = `
+const createProfile = (array) => {
+  let domString = "";
+  array.forEach((user) => {
+    domString += `
   <div class="card" style="width: 18rem;">
-  <img src="https://ummokblog.files.wordpress.com/2017/06/mb1.jpg" class="card-img-top" alt="...">
+  <img src="${user.img}" class="card-img-top" alt="${user.name}">
   <div class="card-body">
-    <h5 class="card-title">Comicus, The Standup Philosopher (C0micus_HotWpt1)</h5>
-    <p class="card-text">I am a standup philosopher, who also dabbles in code in my free time!</p>
+    <h5 class="card-title">${user.name}, ${user.title}</h5>
+    <p class="card-text">${user.bio}</p>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">Rome, Italy</li>
-    <li class="list-group-item">github@C0micus_HotWpt1</li>
+    <li class="list-group-item">${user.location}</li>
+    <li class="list-group-item">${user.gitHubUserName}</li>
   </ul>
   <div class="card-body">
-    <a href="https://www.youtube.com/watch?v=I9QBaKixc5I" class="card-link">Dayjob</a>
-    <a href="https://github.com/nss-evening-cohort-16/gitsub-purple-parrots" class="card-link">GitHub</a>
+    <p>${user.socialMedia[0]}</p>
+    <p>${user.socialMedia[1]}</p>
+    <p>${user.socialMedia[2]}</p>
+    <a href="${user.dayJobLink}" class="card-link">${user.dayJob}</a>
+    <a href="${user.gitHubLink}" class="card-link">${user.gitHub}</a>
   </div>
 </div>
   `;
+  });
   renderToDom("#profile", domString);
 };
 
---
+++ userProfileData.js
@@ -0,0 +1,15 @@
+export const userProfileData = [
+  {
+  name: "Comicus",
+  title: "The Stand Up Philosopher (C0micus_HotWpt1)",
+  location: "Rome, Italy",
+  gitHubUserName: "github@C0micus_HotWpt1",
+  img: "https://ummokblog.files.wordpress.com/2017/06/mb1.jpg",
+  socialMedia: ["Twitter", "Facebook", "Instagram"],
+  bio: "I am a standup philosopher, who also dabbles in code in my free time!",
+  dayJob: "Day Job",
+  dayJobLink: "https://www.youtube.com/watch?v=I9QBaKixc5I",
+  gitHub: "Github",
+  gitHubLink: "https://github.com/nss-evening-cohort-16/gitsub-purple-parrots",
+  },
+];
